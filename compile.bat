abaccb739903b3d68b1441b7ec14a2cf48f4e004
YasserBh <yasserbouhai@gmail.com>
2025-05-13 14:53:38 +0200

    seperate source code an testing files+libraires

---
+++ compile.bat
@@ -0,0 +1,27 @@
+@echo off
+
+echo Creating testing directory if it doesn't exist...
+if not exist testing mkdir testing
+
+echo Compiling Sevens Game...
+g++ -std=c++17 -Wall -Wextra -O3 -Wno-cast-function-type ^
+code_skeleton\MyCardParser.cpp ^
+code_skeleton\MyGameParser.cpp ^
+code_skeleton\MyGameMapper.cpp ^
+code_skeleton\RandomStrategy.cpp ^
+code_skeleton\GreedyStrategy.cpp ^
+code_skeleton\main.cpp ^
+-o testing\sevens_game.exe
+
+echo.
+echo Compiling RandomStrategy DLL...
+g++ -std=c++17 -Wall -Wextra -shared -fPIC -DBUILDING_DLL code_skeleton\RandomStrategy.cpp -o testing\random_strategy.dll
+
+echo.
+echo Compiling GreedyStrategy DLL...
+g++ -std=c++17 -Wall -Wextra -shared -fPIC -DBUILDING_DLL code_skeleton\GreedyStrategy.cpp -o testing\greedy_strategy.dll
+
+
+
+echo.
+echo Done!
---
+++ compile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Compiling Sevens Game..."
+g++ -std=c++17 -Wall -Wextra -O3 \
+code_skeleton/MyCardParser.cpp \
+code_skeleton/MyGameParser.cpp \
+code_skeleton/MyGameMapper.cpp \
+code_skeleton/RandomStrategy.cpp \
+code_skeleton/GreedyStrategy.cpp \
+code_skeleton/main.cpp \
+-o testing/sevens_game -ldl -Wl,-rpath=.
+
+echo ""
+echo "Compiling RandomStrategy SO..."
+g++ -std=c++17 -Wall -Wextra -shared -fPIC -DBUILDING_DLL code_skeleton/RandomStrategy.cpp -o testing/random_strategy.so
+
+echo ""
+echo "Compiling GreedyStrategy SO..."
+g++ -std=c++17 -Wall -Wextra -shared -fPIC -DBUILDING_DLL code_skeleton/GreedyStrategy.cpp -o testing/greedy_strategy.so
+
+
+
+echo ""
+echo "Done!"
